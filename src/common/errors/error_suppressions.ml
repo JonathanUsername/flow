969d43802e88dd69c30caa9f28bd434f50152ef6
Jonathan King <jonathan.j.king@gmail.com>
2018-09-03 14:45:44 +0100

    Fix whitelist separator, and add debugging statement

--- src/common/errors/error_suppressions.ml
+++ src/common/errors/error_suppressions.ml
@@ -126,23 +126,24 @@ let check err suppressions severity_cover unused =
   let is_in_dependency =
     let primary_loc = Errors.loc_of_error err in
     Option.value_map (Loc.source primary_loc) ~default:false ~f:(fun filename ->
-      String_utils.is_substring "/node_modules/" (File_key.to_string filename))
+      String_utils.is_substring "/node_modules/" (File_key.to_string filename) 
+    )
   in
-let is_lint_warning = is_in_dependency && (Option.is_some lint_kind) in
-let is_whitelisted = match Errors.loc_of_error err with
-  | primary_loc ->
+  let is_lint_warning = is_in_dependency && (Option.is_some lint_kind) in
+  let is_whitelisted = 
+    let primary_loc = Errors.loc_of_error err in
     Option.value_map (Loc.source primary_loc) ~default:false ~f:(fun filename ->
       Whitelisting.filename_in_whitelist filename
     )
-in
-let result = match Errors.kind_of_error err with
-| Errors.RecursionLimitError ->
-  (* TODO: any related suppressions should not be considered used *)
-  Err
-| _ -> if is_lint_warning || is_whitelisted
-  then Off (* TODO: this should not show up with --include-suppressed *)
-  else result
-in Some (result, used, unused)
+  in
+  let result = match Errors.kind_of_error err with
+  | Errors.RecursionLimitError ->
+    (* TODO: any related suppressions should not be considered used *)
+    Err
+  | _ -> if is_lint_warning || is_whitelisted
+    then Off (* TODO: this should not show up with --include-suppressed *)
+    else result
+  in Some (result, used, unused)
 
 
 (* Gets the locations of the suppression comments that are yet unused *)
--- src/common/errors/whitelisting.ml
+++ src/common/errors/whitelisting.ml
@@ -2,7 +2,7 @@
 open Sys
 
 let whitelist_env = try getenv "FLOW_WHITELIST" with Not_found -> ""
-let whitelist_re_list = if whitelist_env = "" then [] else Str.split (Str.regexp "++") whitelist_env
+let whitelist_re_list = if whitelist_env = "" then [] else Str.split (Str.regexp ",") whitelist_env
 let matches_whitelist_elem s1 s2 =
     let re = Str.regexp s2
     in
@@ -21,5 +21,20 @@ let to_string = function
 
 (* let in_whitelist elem = is_in_whitelist (to_string elem) *)
 
+
+
 let loc_in_whitelist (elem: Loc.t) = is_in_whitelist (Loc.to_string elem)
+
+(* THIS IS USEFUL TO DEBUG *)
+let debug_print filename = 
+    let file_str = File_key.to_string filename in 
+    (
+        Printf.printf "filename to string: %s\n" (File_key.to_string filename); 
+        Printf.printf "whitelisted: %b\n" (is_in_whitelist file_str); 
+        Printf.printf "whitelist: %s\n" (String.concat " " whitelist_re_list)
+    )
+
+(* let filename_in_whitelist elem = let () = debug_print elem in is_in_whitelist (File_key.to_string elem) *)
+
 let filename_in_whitelist elem = is_in_whitelist (File_key.to_string elem)
+
